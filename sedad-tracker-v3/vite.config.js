c9e27f67be1f3337f0665851cc1e8f873adf8c6b
ahmedouvadel299 <midomed299@gmail.com>
2026-08-19 19:10:26 +0000

    Delete sedad-tracker-v3 directory

--- sedad-tracker-v3/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base: اسم المستودع على GitHub Pages (يُنشر على github.io/اسم-المستودع/)
-export default defineConfig({
-  plugins: [react()],
-  base: '/sedad-tracker-V2/',
-})
