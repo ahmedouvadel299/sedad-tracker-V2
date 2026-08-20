ca3addd0d9f90c99d0575aee9b961737138536cc
ahmedouvadel299 <midomed299@gmail.com>
2026-08-20 01:16:49 +0000

    Add WelcomeSplash route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login.jsx'
+import WelcomeSplash from './pages/WelcomeSplash.jsx'
 import AgentDashboard from './pages/AgentDashboard.jsx'
 import AdminDashboard from './pages/AdminDashboard.jsx'
 import ObserverDashboard from './pages/ObserverDashboard.jsx'
 
-// TODO: استبدال هذا لاحقاً بمنطق حقيقي يقرأ حالة تسجيل الدخول من Firebase Auth
-// ودور المستخدم من roles/{uid} في Realtime Database
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/bienvenue" element={<WelcomeSplash />} />
       <Route path="/agent" element={<AgentDashboard />} />
       <Route path="/admin" element={<AdminDashboard />} />
       <Route path="/observateur" element={<ObserverDashboard />} />
