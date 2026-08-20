223fb83be0fb08616af65c35f3901e4fd70eac2e
ahmedouvadel299 <midomed299@gmail.com>
2026-08-20 23:50:25 +0000

    Change logo import from logo.js to logo.jsx

--- src/pages/WelcomeSplash.jsx
+++ src/pages/WelcomeSplash.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import LogoIcon from './logo.js'
+import LogoIcon from './logo.jsx'
 
 const PHRASES = [
   "Votre régularité fait la force de l'équipe.",
